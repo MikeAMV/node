a8440f42cecc4e4e07b6e84ca83a4c4bdcee1938
MikeAMV <92051208+MikeAMV@users.noreply.github.com>
2022-11-15 10:07:19 -0600

    [ADD] Auth

--- src/config/express.js
+++ src/config/express.js
@@ -1,20 +1,25 @@
 const express = require('express');
 require('dotenv').config(); //imports
 const cors = require('cors');
-
+const {
+  personalRouter,
+  userRouter,
+  authRouter,
+} = require('../modules/controller/routes');
 const app = express(); //Instanciar server
 app.set('port', process.env.PORT || 3000);
-
 //middlewares
-app.use(cors({ origins: '*' }));//Permite recibir cualquier peticion con X origen
-app.use(express.json({ limit: '50mb' }));//Permite peticiones hasta 50Mb
-
+app.use(cors({ origins: '*' })); //Permite recibir cualquier peticion con X origen
+app.use(express.json({ limit: '50mb' })); //Permite peticiones hasta 50Mb
 //Routes
-app.get('/', ( request, response ) => {
+app.get('/', (request, response) => {
   response.send('Bienvenido a la AppRest Personal-UTEZ');
-});//Endpoints
-//http://localhost:3000/ -> Primer Endpoint
-
+}); //Endpoints
+app.use(`/api/personal`, personalRouter);
+app.use(`/api/user`, userRouter);
+app.use(`/api/auth`, authRouter);
+//app.use(`/api/position`, positionRouter);
+//app.use(`/api/user`, userRouter);
 module.exports = {
-  app
+  app,
 }; // {app:app}
